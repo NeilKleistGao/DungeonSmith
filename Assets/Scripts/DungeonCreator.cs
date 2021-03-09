8b6f8e66bb89697101c25a284006b094b53b4eba
NeilKleistGao <neilkleistgao@163.com>
2021-03-09 22:42:09 +0800

    add some shit

--- Assets/Scripts/DungeonCreator.cs
+++ Assets/Scripts/DungeonCreator.cs
@@ -31,7 +31,14 @@ public class DungeonCreator : MonoBehaviour
     [Tooltip("Dungeon prefab.")]
     public GameObject DUNGEON_PREFAB;
     [Tooltip("Prefabs that used to build all kinds of rooms.")]
-    public GameObject[] ROOM_PREFAB;
+    public GameObject[] ROOM_PREFABS;
+    [Tooltip("How many rooms are required for each type.")]
+    public int[] ROOM_NUMBERS;
+
+    private Grid grid;
+
+    private bool[,] occupied;
+    private bool[,] connected;
 
     // Start is called before the first frame update
     void Start()
@@ -53,6 +60,54 @@ public class DungeonCreator : MonoBehaviour
             return;
         }
 
-        // TODO: 
+        grid = DUNGEON_PREFAB.GetComponent<Grid>();
+        if (grid == null)
+        {
+            Debug.Log("Missing grid component in dungeon prefab!");
+            return;
+        }
+
+        if (ROOM_NUMBERS.Length != ROOM_PREFABS.Length)
+        {
+            Debug.Log("Please make sure room numbers and room prefabs have the same length!");
+            return;
+        }
+
+        if (ROOM_NUMBERS.Length > WORLD_WIDTH * WORLD_HEIGHT)
+        {
+            Debug.Log("Too many rooms are specified!");
+            return;
+        }
+
+        int total = 0;
+        foreach (GameObject prefab in ROOM_PREFABS)
+        {
+            var room = prefab.GetComponent<DungeonRoom>();
+            if (room == null)
+            {
+                Debug.Log("Missing DungeonRoom component in dungeon room prefab!");
+            }
+            else
+            {
+                total += room.REQUIRED_IN_LEVEL;
+            }
+        }
+
+        occupied = new bool[WORLD_WIDTH, WORLD_HEIGHT];
+        connected = new bool[WORLD_WIDTH, WORLD_HEIGHT];
+        for (int i = 0; i < WORLD_WIDTH; i++)
+        {
+            for (int j = 0; j < WORLD_HEIGHT; j++)
+            {
+                occupied[i, j] = false;
+                connected[i, j] = false;
+            }
+        }
+
+        occupied[0, 0] = true; connected[0, 1] = true;
+        ArrayList open_list = new ArrayList();
+        open_list.Add(1);
+
+        
     }
 }
--- Assets/Scripts/DungeonRoom.cs
+++ Assets/Scripts/DungeonRoom.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class DungeonRoom : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [Header("Generation Options")]
+    [Tooltip("How many rooms in this form are needed in the map.")]
+    public int REQUIRED_IN_LEVEL;
+
     void Start()
     {
         
